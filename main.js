6dc89df7546b52234b783cdf4015aa048f71942d
Mattias Hellman <mattias.hellman@gmail.com>
2025-09-25 09:58:18 +0200

    Remove trail effect.

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 20) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
@@ -92,7 +92,7 @@ while (balls.length < 25) {
 }
 
 function loop() {
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillStyle = "rgb(0 0 0 / 100%)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
